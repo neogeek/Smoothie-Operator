246aab1e00a58df9cc6ab20783b255e83e775fe6
neogeek <neogeek0101@gmail.com>
2019-10-05 21:10:50 -0400

    Added fruit shadow with distance scaling.

--- Assets/Scripts/FruitController.cs
+++ Assets/Scripts/FruitController.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 public class FruitController : MonoBehaviour
 {
 
+#pragma warning disable CS0649
+    [SerializeField]
+    private LayerMask _truckLayerMask;
+
+    [SerializeField]
+    private Transform _shadowTransform;
+#pragma warning restore CS0649
+
+    private void Update()
+    {
+
+        var hit = Physics2D.Raycast(gameObject.transform.position, Vector3.down, 10f, _truckLayerMask);
+
+        if (hit)
+        {
+
+            _shadowTransform.position = hit.point;
+
+            _shadowTransform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, Mathf.InverseLerp(0, 5, hit.distance));
+
+        }
+
+    }
 
 }
