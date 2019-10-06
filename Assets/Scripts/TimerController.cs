0b3d4a5078b877f6e07372b217896224ba7f8f88
neogeek <neogeek0101@gmail.com>
2019-10-05 20:13:20 -0400

    Added timer controller.

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+
+public class TimerController : MonoBehaviour
+{
+
+#pragma warning disable CS0649
+    [SerializeField]
+    private TimerReference _timerReference;
+#pragma warning restore CS0649
+
+    private Coroutine _timerCoroutine;
+
+    private void Awake()
+    {
+
+        _timerReference.Reset();
+
+        StartTimer();
+
+    }
+
+    public void StartTimer()
+    {
+
+        _timerCoroutine = StartCoroutine(Tick());
+
+    }
+
+    private IEnumerator Tick()
+    {
+
+        while (true)
+        {
+
+            yield return new WaitForSecondsRealtime(1);
+
+            _timerReference.timer += 1;
+
+        }
+
+    }
+
+    public void StopTimer()
+    {
+
+        if (_timerCoroutine == null)
+        {
+            return;
+        }
+
+        StopCoroutine(_timerCoroutine);
+
+        _timerCoroutine = null;
+
+    }
+
+}
