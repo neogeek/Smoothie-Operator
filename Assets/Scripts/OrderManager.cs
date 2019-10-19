16a013dc3f2668a2f8461ae7641a2cfc29246983
neogeek <neogeek0101@gmail.com>
2019-10-19 12:20:00 -0400

    Added OrderManager prefab and component.

---
+++ Assets/Scripts/OrderManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace SmoothieOperator
+{
+
+    public class OrderManager : MonoBehaviour
+    {
+
+        private readonly WaitForSeconds _delayBetweenSpawningNewCustomers = new WaitForSeconds(1);
+
+        [SerializeField]
+        private Transform[] _customerTransforms;
+
+        [SerializeField]
+        private GameObject[] _customerPrefabs;
+
+        private readonly Dictionary<Transform, CustomerController> _customers =
+            new Dictionary<Transform, CustomerController>();
+
+        private IEnumerator Start()
+        {
+
+            while (true)
+            {
+
+                yield return _delayBetweenSpawningNewCustomers;
+
+                if (_customers.Count < _customerTransforms.Length)
+                {
+
+                    SpawnNewCustomer();
+
+                }
+
+            }
+
+        }
+
+        private void SpawnNewCustomer()
+        {
+
+            var spawnTransform = _customerTransforms.First(t => !_customers.ContainsKey(t));
+
+            if (!spawnTransform)
+            {
+                return;
+            }
+
+            var spawnedCustomer = Instantiate(
+                _customerPrefabs[Random.Range(0, _customerPrefabs.Length)],
+                spawnTransform.position,
+                Quaternion.identity,
+                gameObject.transform);
+
+            var customerController = spawnedCustomer.GetComponent<CustomerController>();
+
+            _customers.Add(spawnTransform, customerController);
+
+        }
+
+    }
+
+}
