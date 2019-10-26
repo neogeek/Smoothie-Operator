ae994de5f54ea05387ffbc9ea938779483695911
neogeek <neogeek0101@gmail.com>
2019-10-26 16:36:59 -0400

    Added fruit controller to fruit prefab.

--- Assets/Scripts/FruitController.cs
+++ Assets/Scripts/FruitController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace SmoothieOperator
 {
 
-    public class FruitController
+    public class FruitController : MonoBehaviour
     {
 
         public static LayerMask layerMask => LayerMask.GetMask("Fruit");
