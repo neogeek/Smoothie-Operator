767438b7405228d5b3cfde67e652b87d387b0893
neogeek <neogeek0101@gmail.com>
2019-10-26 11:52:31 -0400

    Added proper bounce to fruits in truck.

--- Assets/Scripts/TruckController.cs
+++ Assets/Scripts/TruckController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace SmoothieOperator
@@ -6,19 +7,59 @@ namespace SmoothieOperator
     public class TruckController : MonoBehaviour
     {
 
-        private static LayerMask _fruitLayerMask => LayerMask.NameToLayer("Fruit");
+        private const float BOUNCE_FORCE = 5;
 
-        private void OnCollisionEnter2D(Collision2D collision)
+        private static LayerMask _fruitLayerMask => LayerMask.GetMask("Fruit");
+
+        [SerializeField]
+        private BoxCollider2D _floorCollider;
+
+        [SerializeField]
+        private AnimationCurve _bounceAnimation;
+
+        private Vector3 _bounceBoxCastPosition;
+
+        private Vector3 _bounceBoxCastSize;
+
+        private void Awake()
         {
 
-            if (!collision.gameObject.layer.Equals(_fruitLayerMask))
+            var colliderBounds = _floorCollider.bounds;
+
+            _bounceBoxCastPosition = colliderBounds.center + Vector3.up * colliderBounds.size.y;
+
+            _bounceBoxCastSize = colliderBounds.size;
+
+        }
+
+        private IEnumerator Start()
+        {
+
+            while (true)
             {
-                return;
+
+                var closeFruits = Physics2D.BoxCastAll(_bounceBoxCastPosition, _bounceBoxCastSize, 0, Vector2.zero, 0,
+                    _fruitLayerMask);
+
+                foreach (var fruit in closeFruits)
+                {
+
+                    fruit.collider.gameObject.GetComponent<Rigidbody2D>()
+                        .AddForce(Vector2.up * BOUNCE_FORCE, ForceMode2D.Impulse);
+
+                }
+
+                yield return new WaitForSeconds(Random.Range(1, 5));
+
             }
 
-            var rigidbody2D = collision.gameObject.GetComponent<Rigidbody2D>();
+        }
 
-            rigidbody2D.velocity = Vector2.Reflect(collision.relativeVelocity, collision.contacts[0].normal);
+        private void OnDrawGizmos()
+        {
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(_bounceBoxCastPosition, _bounceBoxCastSize);
 
         }
 
