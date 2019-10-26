b52027323c4bf620f60bb74aa7e81d3c63e3d370
neogeek <neogeek0101@gmail.com>
2019-10-26 17:34:27 -0400

    Removed magic numbers.

--- Assets/Scripts/ShadowController.cs
+++ Assets/Scripts/ShadowController.cs
@@ -6,6 +6,10 @@ namespace SmoothieOperator
     public class ShadowController : MonoBehaviour
     {
 
+        private const float MAX_RAYCAST_DISTANCE = 10f;
+
+        private const float MAX_SHADOW_SCALE_DISTANCE = 5f;
+
 #pragma warning disable CS0649
         [SerializeField]
         private Transform _shadowTransform;
@@ -14,7 +18,8 @@ namespace SmoothieOperator
         private void Update()
         {
 
-            var hit = Physics2D.Raycast(gameObject.transform.position, Vector3.down, 10f, TruckController.layerMask);
+            var hit = Physics2D.Raycast(gameObject.transform.position, Vector3.down, MAX_RAYCAST_DISTANCE,
+                TruckController.layerMask);
 
             if (!hit)
             {
@@ -24,7 +29,7 @@ namespace SmoothieOperator
             _shadowTransform.position = hit.point;
 
             _shadowTransform.localScale =
-                Vector3.Lerp(Vector3.one, Vector3.zero, Mathf.InverseLerp(0, 5, hit.distance));
+                Vector3.Lerp(Vector3.one, Vector3.zero, Mathf.InverseLerp(0, MAX_SHADOW_SCALE_DISTANCE, hit.distance));
 
         }
 
@@ -33,7 +38,8 @@ namespace SmoothieOperator
 
             Gizmos.color = Color.green;
 
-            var hit = Physics2D.Raycast(gameObject.transform.position, Vector3.down, 10f, TruckController.layerMask);
+            var hit = Physics2D.Raycast(gameObject.transform.position, Vector3.down, MAX_RAYCAST_DISTANCE,
+                TruckController.layerMask);
 
             Gizmos.DrawLine(gameObject.transform.position, hit.point);
 
