145ca49372967e29e71adc2ec0d9772ce9c2b814
neogeek <neogeek0101@gmail.com>
2019-10-26 16:34:02 -0400

    Create FruitController.cs

---
+++ Assets/Scripts/FruitController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace SmoothieOperator
+{
+
+    public class FruitController
+    {
+
+        public static LayerMask layerMask => LayerMask.GetMask("Fruit");
+
+    }
+
+}
