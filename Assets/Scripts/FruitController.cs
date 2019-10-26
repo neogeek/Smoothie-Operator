7f880083f3c43459ff3cf480b18bf205e4e2f8e4
neogeek <neogeek0101@gmail.com>
2019-10-26 16:42:50 -0400

    Added sprites to prefab fruit struct.

--- Assets/Scripts/FruitController.cs
+++ Assets/Scripts/FruitController.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 namespace SmoothieOperator
 {
 
+    [SelectionBase]
     public class FruitController : MonoBehaviour
     {
 
         public static LayerMask layerMask => LayerMask.GetMask("Fruit");
+
+        public Fruit fruit;
 
     }
 
