8c5697547d122ba83624a8cd97c1ead4ef92ef79
neogeek <neogeek0101@gmail.com>
2019-11-23 12:58:05 -0500

    Added nonserialized tag to lives and score.

--- Assets/Scripts/ScriptableObjects/PlayerReference.cs
+++ Assets/Scripts/ScriptableObjects/PlayerReference.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace SmoothieOperator
@@ -9,19 +10,12 @@ namespace SmoothieOperator
 
         public const int STARTING_LIVES = 3;
 
+        [NonSerialized]
         [RangeAttribute(0, STARTING_LIVES)]
         public int lives = STARTING_LIVES;
 
+        [NonSerialized]
         public int score;
-
-        public void Reset()
-        {
-
-            lives = STARTING_LIVES;
-
-            score = 0;
-
-        }
 
     }
 
