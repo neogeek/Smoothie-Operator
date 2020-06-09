e2fc94ba905a22647cae0f7c882a34595e641d03
Scott Doxey <neogeek0101@gmail.com>
2020-06-09 02:11:25 -0400

    Updated title and controls scene.
    Added credits scene.

--- Assets/Scripts/ControlsSceneController.cs
+++ Assets/Scripts/ControlsSceneController.cs
@@ -1,11 +1,28 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace SmoothieOperator
 {
 
     public class ControlsSceneController : MonoBehaviour
     {
+
+        [SerializeField]
+        private Image _controllerControlsImage;
+
+        [SerializeField]
+        private Image _keyboardControlsImage;
+
+        private bool ControllerConnected => Input.GetJoystickNames().Length > 0;
+
+        private void Start()
+        {
+
+            _controllerControlsImage.enabled = ControllerConnected;
+            _keyboardControlsImage.enabled = !ControllerConnected;
+
+        }
 
         private void Update()
         {
